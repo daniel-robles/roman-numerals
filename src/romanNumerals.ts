72f8c2631ef5c7ca3cb5e9884a84d14d46c9a195
Daniel Robles <drobles@absencesoft.com>
2026-05-28 08:15:49 -0400

    First scenario: Base symbols, minimum code, green

--- src/romanNumerals.ts
+++ src/romanNumerals.ts
@@ -1,5 +1,12 @@
-export function toRoman(n: number): string {
-  throw new Error('Not implemented');
+export function toRoman(number: number): string {
+  if (number === 1) return 'I';
+  if (number === 5) return 'V';
+  if (number === 10) return 'X';
+  if (number === 50) return 'L';
+  if (number === 100) return 'C';
+  if (number === 500) return 'D';
+  if (number === 1000) return 'M';
+  return "";
 }
 
 export function fromRoman(s: string): number {
