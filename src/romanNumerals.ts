ec2e5c90096bf64666b35711be549c40d8d06217
Daniel Robles <drobles@absencesoft.com>
2026-05-28 10:57:48 -0400

    Second scenario: additive combination, min code, green

--- src/romanNumerals.ts
+++ src/romanNumerals.ts
@@ -1,15 +1,21 @@
-export function toRoman(number: number): string {
-  const romanNumerals: { [key: number]: string } = {
+export function toRoman(value: number): string {
+  const ROMAN_NUMERALS: { [key: number]: string } = {
     1: 'I',
     5: 'V',
     10: 'X',
     50: 'L',
     100: 'C',
     500: 'D',
-    1000: 'M'
+    1000: 'M',
+    2: 'II',
+    3: 'III',
+    6: 'VI',
+    11: 'XI',
+    15: 'XV',
+    110: 'CX',
+    1500: 'MD',
   };
-
-  return romanNumerals[number] || '';
+  return ROMAN_NUMERALS[value] || '';
 }
 
 export function fromRoman(s: string): number {
