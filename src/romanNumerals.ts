f4e3e57b1f9973cf382c19e265ff3fc197322284
Daniel Robles <drobles@absencesoft.com>
2026-05-28 11:25:21 -0400

    Third scenario: subtractive combination, min code, green

--- src/romanNumerals.ts
+++ src/romanNumerals.ts
@@ -1,11 +1,17 @@
 export function toRoman(value: number): string {
   const ROMAN_NUMERALS: [number, string][] = [
     [1000, 'M'],
+    [900,  'CM'],  // ← agregar
     [500,  'D'],
+    [400,  'CD'],  // ← agregar
     [100,  'C'],
+    [90,   'XC'],  // ← agregar
     [50,   'L'],
+    [40,   'XL'],  // ← agregar
     [10,   'X'],
+    [9,    'IX'],  // ← agregar
     [5,    'V'],
+    [4,    'IV'],  // ← agregar
     [1,    'I'],
   ];
 
